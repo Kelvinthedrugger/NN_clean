2a1579a57d1317a424ba9ce11ad508292e15e16f
Kelvinthedrugger <890225kelvin@gmail.com>
2021-10-10 23:52:53 +0800

    conv2d forward done

--- test_conv.py
+++ test_conv.py
@@ -1,4 +1,4 @@
-from module import Tensor, layer_init
+from module import layer_init
 import numpy as np
 
 
@@ -9,19 +9,24 @@ class Conv:
         self.weight = layer_init(kernel_size, kernel_size)
         self.stride = stride
         self.padding = padding  # bool
+        self.forward = None
+        self.grad = None
+        self.trainable = True
 
     def __call__(self, x):
         rows, cols = x.shape
-        out = np.zeros((rows-2, cols-2), dtype=np.float32)
-        for i in range(0, (rows-self.kernel_size)//self.stride + 1, self.stride):
-            for j in range(0, (cols-self.kernel_size)//self.stride+1, self.stride):
-                out[i][j] = j+i*3
-
+        out = np.zeros((self.filters, rows-2, cols-2), dtype=np.float32)
+        for r in range(self.filters):
+            for k in range(0, (rows-self.kernel_size)//self.stride + 1, self.stride):
+                for m in range(0, (cols-self.kernel_size)//self.stride+1, self.stride):
+                    tmp = x[k:k+self.kernel_size, m:m+self.kernel_size]
+                    out[r, k, m] = np.multiply(self.weight, tmp).sum()
+        del tmp
         return out
 
 
 if __name__ == "__main__":
     np.random.seed(1337)
     layer = Conv(1, 3)
-    x = layer_init(5, 5)
-    print(layer(x))
+    x = np.random.uniform(-1, 1, size=(5, 5))
+    out = layer(x)
