b8ff565fc604afb0e55c18ae55c83d3238bd87a8
Kelvinthedrugger <890225kelvin@gmail.com>
2021-10-15 01:35:57 +0800

    add file

---
+++ model_topo.py
@@ -0,0 +1,11 @@
+from module import Tensor
+import numpy as np
+
+
+class Model:
+    def __init__(self):
+        """
+        construct topo available model here
+        , consider started from layer class
+        """
+        pass
