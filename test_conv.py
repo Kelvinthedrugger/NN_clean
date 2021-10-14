10a6e740e69d983356497c3a98080dca5a9d6f09
Kelvinthedrugger <890225kelvin@gmail.com>
2021-10-14 11:22:08 +0800

    profile of conv

--- test_conv.py
+++ test_conv.py
@@ -1,5 +1,4 @@
-from module import layer_init, Tensor
-from module.Loss import mse
+from module import layer_init, Tensor, Loss, Optimizer
 import numpy as np
 
 
@@ -25,30 +24,48 @@ class Conv:
         self.padding = padding  # bool
         # below similar to Tensor
         self.forward = None
-        self.grad = [[] for _ in range(filters)]
+        self.grad = np.zeros_like(weight)  # zeros with same shape as weight
         self.trainable = True
 
     def __call__(self, x):
+        """add padding"""
         rows, cols = x.shape
         out = np.zeros((self.filters, rows-2, cols-2), dtype=np.float32)
         # now, it's fake gradient
         for r in range(self.filters):
-            grad = 0
+            #grad = 0
             for k in range(0, (rows-self.kernel_size)//self.stride + 1, self.stride):
                 for m in range(0, (cols-self.kernel_size)//self.stride+1, self.stride):
                     tmp = x[k:k+self.kernel_size, m:m+self.kernel_size]
                     ret = np.multiply(self.weight[r], tmp)
                     out[r, k, m] = ret.sum()
-                    grad += tmp.T @ ret  # gradient wrt filter
-            self.grad[r].append(grad)
+                    # grad += tmp.T @ ret  # gradient wrt filter
+            # self.grad[r].append(grad)
         return out
 
 
 if __name__ == "__main__":
     np.random.seed(1337)
-    layer = Conv(filters=2, kernel_size=3)
-    x = np.random.uniform(-1, 1, size=(5, 5))
-    out = layer(x)
-    print(out)
-    print(layer.grad[0])
-    print(layer.grad[1])
+    from matplotlib.pyplot import imshow, show
+    # five
+    x = np.array(
+        [[0, 0, 1, 1, 1, 0, 0], [0, 0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0]])
+    print(x.shape)
+    x = np.concatenate(
+        [[[ele]*4 for ele in row]*4 for row in x]).reshape(28, 28)
+    print(x.shape)
+    # layer init
+    layer = Conv(filters=1, kernel_size=3)  # filters=1 means nothing
+    lossfn = Loss().mse
+    optim = Optimizer(learning_rate=1e-3).SGD
+    # training loop
+    losses = []
+    for _ in range(2):
+        # forward pass
+        out = layer(x)
+        # backprop
+        loss, gradient = lossfn(x, out)
+        layer.grad = x.T @ gradient
+        optim(layer)
+        losses.append(loss)
+    print(losses)
