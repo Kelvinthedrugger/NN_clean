76c4422d636b47101ac53c481f3bd7dd408e9676
Kelvinthedrugger <890225kelvin@gmail.com>
2021-10-30 19:05:50 +0800

    convolution gradient descent worked!

--- test_conv.py
+++ test_conv.py
@@ -32,7 +32,7 @@ class Conv:
     def __call__(self, x):
         """add padding"""
         rows, cols = x.shape
-        out = np.zeros((self.filters, rows-2, cols-2), dtype=np.float32)
+        out = np.zeros((self.filters, rows, cols), dtype=np.float32)
         for r in range(self.filters):
             for k in range(0, (rows-self.kernel_size)//self.stride + 1, self.stride):
                 for m in range(0, (cols-self.kernel_size)//self.stride+1, self.stride):
@@ -44,26 +44,48 @@ class Conv:
 
 if __name__ == "__main__":
     np.random.seed(1337)
-    from matplotlib.pyplot import imshow, show
+    from matplotlib import pyplot as plt
     # five
     x = np.array(
         [[0, 0, 1, 1, 1, 0, 0], [0, 0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0]])
-    print(x.shape)
     x = np.concatenate(
         [[[ele]*4 for ele in row]*4 for row in x]).reshape(28, 28)
-    print(x.shape)
     # layer init
     layer = Conv(filters=1, kernel_size=3)  # filters=1 means nothing
     lossfn = Loss().mse
     optim = Optimizer(learning_rate=1e-3).SGD
     # training loop
     losses = []
-    for _ in range(2):
+    out = None
+    for _ in range(10):
         # forward pass
         out = layer(x)
+        assert out.shape == (1, 28, 28)
         # backprop
-        loss, gradient = lossfn(x, out)
-        layer.grad = x.T @ gradient
+        loss, gradient = lossfn(x, out, supervised=False)
+        # tip: shape not aligned!
+        # layer.grad = x.T @ gradient  # wrong?
+        tmpgrad = (x.T @ gradient).reshape((28, 28))
+        # do conv update
+        tmpker = np.zeros((3, 3), dtype=np.float32)
+        for r in range(1):
+            for k in range(0, (x.shape[0]-3)//1+1, 3):
+                for m in range(0, (x.shape[1]-3)//1+1, 3):
+                    tmpker += tmpgrad[k:k+3, m:m+3]
+        layer.grad = tmpker
+        # updated weights to each filter not done
         optim(layer)
-        losses.append(loss)
-    print(losses)
+        losses.append(loss.mean())
+
+    print("epoch  loss")
+    for i in range(len(losses)):
+        print("%d      %.4f" % (i, losses[i]))
+
+    #plt.figure(figsize=(5, 10))
+    plt.subplot(1, 2, 1)
+    plt.imshow(x.reshape(28, 28))
+    plt.title("original image")
+    plt.subplot(1, 2, 2)
+    plt.imshow(out.reshape(28, 28))
+    plt.title("forward pass after training")
+    plt.show()
