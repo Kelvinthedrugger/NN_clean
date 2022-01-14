31514cf4fb8959bb6bee6e9fb36c612f57a18eb8
Kelvinthedrugger <890225kelvin@gmail.com>
2022-01-14 14:13:15 +0800

    developing flatten layer

--- test_conv.py
+++ test_conv.py
@@ -1,5 +1,5 @@
 from nn.module import Loss 
-from nn.topo import ReLU, Conv, Optimizer 
+from nn.topo import ReLU, Conv, Optimizer, Linear
 from matplotlib import pyplot as plt
 
 import numpy as np
@@ -10,6 +10,81 @@ def plot_filters(weight):
     plt.imshow(weight.weight[r] * 100 + 100)
   plt.show()
 
+
+class Flatten()
+
+
+def debug():
+  # five
+  x = np.array(
+      [[0, 0, 1, 1, 1, 0, 0], [0, 0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0]])
+  x = np.concatenate(
+      [[[ele]*4 for ele in row]*4 for row in x]).reshape(1, 28, 28)
+
+  # pure conv model
+  layer1 = Conv(filters=3, kernel_size=3,stride=2)
+  act = ReLU()
+  layer = Conv(filters=5, kernel_size=5, stride=1)
+  layer(act(layer1))
+  """
+  # conv-dense model (DNN ? )
+  layer1 = Conv()
+  act1 = ReLU()
+  layer2 = Conv()
+  layer3 = Flatten()
+  layer4 = Linear()
+  layer4(layer3(layer2(act1(layer1))))
+  """
+  lossfn = Loss().mse
+  opt = Optimizer(2e-6).SGD
+
+  losses = []
+  # training loop
+  from time import time
+  start = time()
+  for i in range(10):
+   out = layer.forwards(x)
+
+   loss, grad = lossfn(out,x,supervised=False)
+   # callbacks: early stop
+   if len(losses) > 0 and loss.mean() > losses[-1]: break
+
+   losses.append(loss.mean())
+
+   # callbacks: early stop also
+   if losses[-1] > 0.5: break
+
+   layer.backwards(grad)
+   opt(layer)
+  end = time()
+  # print model
+  print(layer, layer1)
+ 
+  for i in range(len(losses)):
+    print("epoch: %d, loss: %.4f" % (i, losses[i]))
+  print("total time: %.4f, time per epoch: %.4f" % (end-start, (end-start)/(i+1)))
+
+  """
+  # input/output
+  plt.subplot(1,2,1)
+  plt.imshow(x[0])
+  plt.subplot(1,2,2)
+  plt.imshow(out[0])
+  plt.show()"""
+  # filters
+  #plot_filters(layer)
+  #plot_filters(layer1)
+
+
+
+if __name__ == "__main__":
+  np.random.seed(1337)
+  debug()
+  #large()
+
+
+
+"""
 # train on mnist dataset
 def large():
   from fetch_it import mnist
@@ -56,62 +131,4 @@ def large():
     print("epoch: %d, loss: %.4f" % (i, losses[i]))
   print("total time: %.4f, time per epoch: %.4f" % (end-start, (end-start)/(i+1)))
 
-def debug():
-  # five
-  x = np.array(
-      [[0, 0, 1, 1, 1, 0, 0], [0, 0, 1, 0, 0, 0, 0], [0, 0, 1, 1, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 1, 0], [0, 0, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0]])
-  x = np.concatenate(
-      [[[ele]*4 for ele in row]*4 for row in x]).reshape(1, 28, 28)
-
-  layer1 = Conv(filters=3, kernel_size=3,stride=2)
-  layer = Conv(filters=5, kernel_size=5, stride=1)
-  layer(layer1)
-
-  lossfn = Loss().mse
-  opt = Optimizer(2e-6).SGD
-
-  losses = []
-
-  # training loop
-  from time import time
-  start = time()
-  for i in range(10):
-   out = layer.forwards(x)
-
-   loss, grad = lossfn(out,x,supervised=False)
-   # callbacks: early stop
-   if len(losses) > 0 and loss.mean() > losses[-1]: break
-
-   losses.append(loss.mean())
-
-   # callbacks: early stop also
-   if losses[-1] > 0.5: break
-
-   layer.backwards(grad)
-   opt(layer)
-  end = time()
-  # print model
-  print(layer, layer1)
- 
-  for i in range(len(losses)):
-    print("epoch: %d, loss: %.4f" % (i, losses[i]))
-  print("total time: %.4f, time per epoch: %.4f" % (end-start, (end-start)/(i+1)))
-
-  """
-  # input/output
-  plt.subplot(1,2,1)
-  plt.imshow(x[0])
-  plt.subplot(1,2,2)
-  plt.imshow(out[0])
-  plt.show()"""
-  # filters
-  #plot_filters(layer)
-  #plot_filters(layer1)
-
-
-
-if __name__ == "__main__":
-  #np.random.seed(1337)
-  #debug()
-  large()
-
+"""
