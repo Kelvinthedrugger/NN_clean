8b8a61a23e018395c95ca2fa9eebea51c3fec5b5
Kelvinthedrugger <890225kelvin@gmail.com>
2022-02-06 03:01:33 +0800

    raw method works

--- fun/fit_line.py
+++ fun/fit_line.py
@@ -4,16 +4,52 @@ curdir = os.path.dirname(os.path.realpath(__file__))
 pardir = os.path.dirname(curdir)
 sys.path.append(pardir)
 
-from nn.module import Loss, Optimizer
-from nn.topo import ReLU, Linear
+import nn.module as nn
 import numpy as np
-
+from nn.topo import Linear
 
 # dataset: y = a*x + b, (a,b) = (1,2) in this case
-x_train = np.arange(0.1,10,0.1)
+x_train = np.arange(0.1,10.1,0.1)
 x_train = x_train.reshape((len(x_train),1,1))
 y_train = x_train + 2
 
+# from scratch
+layer1 = Linear(1,10)
+layer2 = Linear(10,1)
+
+lossfn = nn.Loss().mse
+
+# forward pass and backprop
+def fb(x,y,lr=1e-4):
+  f1 = layer1.forwards(x) 
+  f11 = np.maximum(f1,0)
+  f2 = layer2.forwards(f11) 
+
+  #print(f2.shape)
+
+  # backprop
+  loss, grad = lossfn(y,f2,supervised=False)
+  df2 =  f2.T @ grad 
+  dfr = grad @ (layer2.weight.T)
+  df11 = (dfr > 0.).astype(np.float32)
+  df1 = x.T @ df11
+
+  # update
+  layer2.weight -= lr * df2
+
+  layer1.weight -= lr * df1
+
+  return loss
+
+loss = []
+for _ in range(2000):
+  loss.append(fb(x_train[0],y_train[0],lr=1e-2))
+
+for i in range(9, len(loss),50):
+  print("epoch: %d, loss: %.6f" % (i, loss[i]))
+
+#import matplotlib.pyplot as plt
+#plt.show()
 # we'll pick arbitrary point on the line as test dataset. right now, we'll start proving the ability to fit an arbitrary ds now
 
 # the neural net
@@ -29,36 +65,4 @@ y_train = x_train + 2
 # therefore, asking for (a,b) is usually not the problem we're dealing with
 # since the patterns in real world is just to complicated to be solved analytically
 
-layer1 = Linear(1,20)
-act = ReLU()
-layer2 = Linear(20,1)
-layer2(act(layer1))
-
-lossfn = Loss().mse
-optim = Optimizer(learning_rate=1e-6).Adam
-print(x_train.shape, y_train.shape)
-
-print(x_train[0].shape, y_train[0].shape)
-
-bs = 1 
-loss = []
-print(np.abs(layer1.weight).sum())
-
-for epoch in range(10):
-  #buggy
-  idx = np.random.randint(0,len(x_train),size=bs)
-  print(idx,idx.shape)
-
-  out = layer2.forwards(x_train[idx])
-
-  losss, grad = lossfn(y_train[idx], out,supervised=False)
-  layer2.backwards(grad,optim)
-  loss.append(losss.sum())
-
-print(np.abs(layer1.weight).sum())
-
-for i in range(len(loss)):
-  print("epoch: %d, loss: %.4f" % (i+1,loss[i]))
-
-#from matplotlib import pyplot as plt
 
