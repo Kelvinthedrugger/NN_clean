972b5563f257b69ae311e9dd62ebd501beab39d1
Kelvinthedrugger <890225kelvin@gmail.com>
2022-02-05 15:57:26 +0800

    too buggy

--- fun/fit_line.py
+++ fun/fit_line.py
@@ -12,28 +12,27 @@ import numpy as np
 
 # dataset: y = a*x + b, (a,b) = (1,2) in this case
 x_train = np.arange(0.1,10,0.1)
-x_train = np.expand_dims(x_train,axis=-1) 
-x_train = np.expand_dims(x_train,axis=-1) 
+x_train = x_train.reshape((len(x_train),1,1)
 y_train = x_train + 2
 
 # we'll pick arbitrary point on the line as test dataset. right now, we'll start proving the ability to fit an arbitrary ds now
 
 # the neural net
-# target: learn (a,b)
-layer1 = Linear(1,2)
-act = ReLU()
-layer2 = Linear(2,2)
-layer2(act(layer1))
+# target: learn (a,b).T, which happens to be 
+# , the params in this only layer of the model
+layer = Linear(1,2)
 
 lossfn = Loss().mse
 optim = Optimizer(learning_rate=1e-2).SGD
 
-bs = 4 
+bs = 1 
 loss = []
+print(layer1.weight)
 
 for epoch in range(10):
+  #buggy
   #idx = np.random.randint(0,len(x_train),size=bs)
-  idx = 0
+  idx = epoch 
   #print(layer1.weight)
   #print(x_train[0].shape,y_train[0].shape)
 
@@ -46,17 +45,10 @@ for epoch in range(10):
   #print(layer1.weight)
   loss.append(losss.sum())
 
+print(layer1.weight)
+
 for i in range(len(loss)):
   print("epoch: %d, loss: %.4f" % (i+1,loss[i]))
 
 from matplotlib import pyplot as plt
-# loss wrt epoch/batch
-#plt.plot(list(range(len(loss))), loss)
-
-# on test set
-xt = np.arange(11.,20.,1.)
-xt = np.expand_dims(xt,axis=-1)
-xt = np.expand_dims(xt,axis=-1)
-yt = xt + 2
-
 
